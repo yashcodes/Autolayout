5ca0830ea7637afd0ff582e7579b2a1d6a0a5976
Yash <yash@Yashs-MacBook-Pro.local>
2021-08-02 14:41:38 +0530

    Second screen added

---
+++ AutoLayout2/DetailsViewController.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class DetailsViewController: UIViewController{
+    
+    @IBOutlet weak var genderOfPerson: UILabel!
+    
+    @IBOutlet weak var date: UILabel!
+}
--- AutoLayout2/ViewController.swift
+++ AutoLayout2/ViewController.swift
@@ -15,6 +15,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var nextButton: UIButton!
     
+    var gender = "No gender selected"
+    var strDate = ""
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+            let dateFormatter = DateFormatter()
+
+            dateFormatter.dateStyle = DateFormatter.Style.short
+            dateFormatter.timeStyle = DateFormatter.Style.short
+
+            strDate = dateFormatter.string(from: datePicker.date)
+    }
+    
+    @IBAction func genderSelected(_ sender: UIButton) {
+        gender = sender.titleLabel?.text ?? "No gender selected"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imgView1.layer.cornerRadius = 10
@@ -28,9 +47,16 @@ class ViewController: UIViewController {
         
         nextButton.layer.cornerRadius = 10
         nextButton.clipsToBounds = true
-        
     }
-
-
+    
+    @IBAction func nextClicked(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "showDetails", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! DetailsViewController
+        destinationVC.genderOfPerson.text = gender
+        destinationVC.date.text = strDate
+    }
 }
 
